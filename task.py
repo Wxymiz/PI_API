387c5ac387f15164769b154d7307ae7aee320359
Wxymiz <119953874+Wxymiz@users.noreply.github.com>
2025-11-25 21:30:10 +0100

    implemented pi calculation algorithm

--- task.py
+++ task.py
@@ -1,4 +1,6 @@
 from celery import Celery
+from decimal import Decimal, getcontext
+import time
 
 celery_app = Celery(
     "pi_tasks",
@@ -8,5 +10,27 @@ celery_app = Celery(
 
 @celery_app.task(bind=True)
 def calculate_pi(self, n):
+    """
+    Calculates PI using the Nilakantha series (improved Leibniz series), with Decimal precision.
+    """
 
-    return "Something"
+    # Setting precision of deciaml (larger than n to avoid errors)
+    getcontext().prec = n + 5
+
+    pi = Decimal(3)
+    sign = 1
+
+    total_iterations = n * 5
+
+    for i in range(1, total_iterations):
+        term = Decimal(4) / (Decimal(2*i) * Decimal(2*i + 1) * Decimal(2*i + 2))
+        pi += Decimal(sign) * term
+        sign *= -1
+
+        # Update Celery data
+        self.update_state(
+            state="PROGRESS",
+            meta={"progress": i / total_iterations}
+        )
+
+    return f"{pi:.{n}f}"
