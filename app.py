1cb6803561ae8e7e1f74dadd4339ac5e8b5efb2a
Wxymiz <119953874+Wxymiz@users.noreply.github.com>
2025-11-26 13:12:18 +0100

    endpoints works in app.py

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from celery.result import AsyncResult
+from tasks import celery_app, calculate_pi
 
 app = FastAPI(title="Async Pi Calculator API")
 
-@app.get("/")
-def read_root():
-    return {"message": "Async Pi Calculator API is running"}
+
+@app.get("/calculate_pi")
+def start_calculation(n: int):
+    """Starts async calculation and returns task ID."""
+    task = calculate_pi.delay(n)
+    return {"task_id": task.id}
+
+
+@app.get("/check_progress")
+def check_progress(task_id: str):
+    """Returns current progress or final result of calculation."""
+    task = AsyncResult(task_id, app=celery_app)
+
+    if task.state == "PENDING": # it could be only PROGRESS or FINISHED for state (you wrote that in Task#1) so I changed PENDING to PROGRESS
+        return {"state": "PROGRESS", "progress": 0, "result": None}
+
+    if task.state == "PROGRESS":
+        return {
+            "state": "PROGRESS",
+            "progress": task.info.get("progress"),
+            "result": None
+        }
+
+    if task.state == "SUCCESS":
+        return {"state": "FINISHED", "progress": 1.0, "result": task.get()}
+
+    return {"state": task.state}
